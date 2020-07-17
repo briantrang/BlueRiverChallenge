abc523bd222a7d5c4a1de9a2a7a4aedabef60571
Brian <briantrang@ymail.com>
2020-07-17 11:22:34 -0700

    Second Commit

--- src/App.js
+++ src/App.js
@@ -28,7 +28,11 @@ function App() {
       <h1>Index</h1>
       <hr />
 
-      {/* Apple Park  Section       */}
+      {/* 
+      ###################
+      Apple Park Section  
+      ###################     
+      */}
 
       <h2>Apple Park</h2>
       <ul>
@@ -51,7 +55,11 @@ function App() {
       </ul>
       <hr />
 
-      {/* Bubbs / Results Way Section       */}
+      {/* 
+      ###################
+       Bubbs / Results Way Section   
+      ###################     
+      */}
 
       <h2>Bubb / Results Way</h2>
       <ul>
@@ -75,7 +83,11 @@ function App() {
       </ul>
       <hr />
 
-      {/* Infinite Loop Section       */}
+      {/* 
+      ###################
+      Infinite Loop Section  
+      ###################     
+      */}
 
       <h2>Infinite Loop</h2>
       <ul>
@@ -99,8 +111,11 @@ function App() {
       </ul>
       <hr />
 
-      {/* Santa Clara/San Jose Section       */}
-
+      {/* 
+      ###################
+       Santa Clara/San Jose Section  
+      ###################     
+      */}
       <h2>Santa Clara/San Jose</h2>
       <ul>
         {allItems.map((item, i) => {
@@ -121,7 +136,12 @@ function App() {
         })}
       </ul>
       <hr />
-      {/* Sunnyvale East Section       */}
+
+      {/* 
+      ###################
+       Sunnyvale East Section   
+      ###################     
+      */}
 
       <h2>Sunnyvale East</h2>
       <ul>
@@ -143,8 +163,12 @@ function App() {
         })}
       </ul>
       <hr />
-      {/* Sunnyvale West Section       */}
 
+      {/* 
+      ###################
+       Sunnyvale West Section  
+      ###################     
+      */}
       <h2>Sunnyvale West</h2>
       <ul>
         {allItems.map((item, i) => {
@@ -165,7 +189,12 @@ function App() {
         })}
       </ul>
       <hr />
-      {/* Other Bay Area Section       */}
+
+      {/* 
+      ###################
+       Other Bay Area Section 
+      ###################     
+      */}
 
       <h2>Other Bay Area</h2>
       <ul>
